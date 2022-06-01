c618ce50b20f5143e97cb05689e344c4bb76d9fe
nab542 <mohammednabil91234@gmail.com>
2022-06-01 16:56:48 +0100

    Initial commit

---
+++ src/a.java
@@ -0,0 +1,46 @@
+public class a {
+    return "The game is over."
+            + winner + " has won with the result: Dark - " + darks + " Light - " + lights;
+// line 122 movechcker closing speech mark missing
+
+        for (int {} dir : DIRS)
+// line 120 cell file missing {}
+
+            potentialMoves.remove(cell);
+    // line 80 move chekcer file . remove changed to .add
+
+        if ((cell != null
+// line 129 cell file exclamation mark was removed between brackets. it was not needed
+
+            this.value= value
+// line 72 cell file should be this instead of it being other way around.
+
+        for (int i = 0; i < cells.size() -; i++){
+        // line 95 movechecker -1 was removed
+
+        + winner + " has won with the result: Light - " + darks + " Dark - " + lights;
+        // Movechcker line 123 light and dark had to be switched.
+        return "The game is over."
+                + winner + " has won with the result: Dark - " + darks + " Light - " + lights;
+// line 122 movechcker closing speech mark missing
+
+        for (int {} dir : DIRS)
+// line 120 cell file missing {}
+
+        potentialMoves.remove(cell);
+        // line 80 move chekcer file . remove changed to .add
+
+        if ((cell != null
+// line 129 cell file exclamation mark was removed between brackets. it was not needed
+
+        this.value= value
+// line 72 cell file should be this instead of it being other way around.
+
+        for (int i = 0; i < cells.size() -; i++){
+            // line 95 movechecker -1 was removed
+
+            + winner + " has won with the result: Light - " + darks + " Dark - " + lights;
+            // Movechcker line 123 light and dark had to be switched.
+
+
+    }
